22782607fd9ed168ee2a75e3517f2fd71b11bdf4
Prafful Kumar <92020279+Pk7372singh@users.noreply.github.com>
2024-10-20 21:11:12 +0530

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
